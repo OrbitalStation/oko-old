72e3e8ed4dcca4a01e598c143d08b7382ca1853f
Roman Tarasenko <romann.tarasenko@gmail.com>
2022-10-05 11:35:02 +0600

    Add check for non-return exprs in fn body to have `()` type

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
 #![feature(try_trait_v2)]
 #![feature(adt_const_params)]
 #![feature(generic_associated_types)]
+#![feature(iter_advance_by)]
 
 #![allow(incomplete_features)]
 
-modules!(token ast span error);
+modules!(token ast span error handlers);
 
 pub const SPACES_IN_TAB: u32 = 4;
 
